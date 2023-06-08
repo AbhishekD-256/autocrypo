c53c1caeafabe83da5a99e1ebce963fbc8a98b72
Abhishek Dolai <itsabhi183@gmail.com>
2023-06-08 13:49:30 +0530

    fix tesitimonial section and add styles to navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,20 +10,3 @@ function openNavbar() {
   mainNav.classList.toggle("open");
 }
 menuBtn.addEventListener("click", openNavbar);
-
-pricingToggle.addEventListener("click", function (e) {
-  const element = e.target;
-  if (element.classList.contains("monthly")) {
-    console.log("monthly");
-    prices.forEach((price) =>
-      console.log(+price.textContent.replace(/\D/g, ""))
-    );
-  } else if (element.classList.contains("yearly")) {
-    console.log("yearly");
-    prices.forEach((price) => {
-      price.textContent = +price.textContent.replace(/\D/g, "") * 12;
-    });
-  } else {
-    return;
-  }
-});

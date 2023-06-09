4119507c4bc35222b7fa2c227a95aac2ddd25ea6
Abhishek Dolai <itsabhi183@gmail.com>
2023-06-09 13:29:18 +0530

    add slider functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const menuBtn = document.querySelector(".js-menu-btn");
 const mainNav = document.querySelector(".js-nav");
 const header = document.querySelector("header");
+
 const pricingToggle = document.querySelector(".pricing-toggler");
 const monthBtn = document.querySelector(".monthly");
 const yearBtn = document.querySelector(".yearly");
@@ -9,12 +10,18 @@ const pricePeriods = document.querySelectorAll(".price-period");
 const priceCardBtns = document.querySelectorAll(".js-trail-btn");
 const pricingCards = document.querySelectorAll(".pricing__card");
 
+const dotsWrapper = document.querySelector(".dot-wrap");
+
+//------responsive navbar-----------//
+
 function openNavbar() {
   menuBtn.classList.toggle("open");
   mainNav.style.top = `${header.getBoundingClientRect().height}px`;
   mainNav.classList.toggle("open");
 }
 menuBtn.addEventListener("click", openNavbar);
+
+//-------Pricing Card-----------//
 
 const monthlyPlan = [10, 29, 49];
 function togglePlans(e) {
@@ -48,3 +55,44 @@ priceCardBtns.forEach((btn, i) => {
     pricingCards[i].classList.add("active");
   });
 });
+
+//-------- testimonial cards slider---------//
+
+const testimonialCards = document.querySelectorAll(".testimonials__card");
+const dots = document.querySelectorAll(".slider-dot");
+
+function slideTesimonials(curCard) {
+  testimonialCards.forEach((card, i) => {
+    // card.addEventListener("mouseover");
+    card.classList.remove("active");
+    card.style.transform = `translateX(-${100 * curCard}%)`;
+    dots[i].classList.remove("active");
+  });
+  document.querySelector(`.js-card--${curCard}`).classList.add("active");
+  document.querySelector(`.dot--${curCard}`).classList.add("active");
+}
+
+let currentCard = 0;
+dotsWrapper.addEventListener("click", function (e) {
+  if (!e.target.classList.contains("slider-dot")) return;
+  resetSetInterval();
+  currentCard = +e.target.dataset.dot;
+  slideTesimonials(currentCard);
+});
+
+function autoSlideCards() {
+  currentCard++;
+  console.log(currentCard);
+  if (currentCard >= testimonialCards.length) {
+    currentCard = -1;
+  } else {
+    slideTesimonials(currentCard);
+  }
+}
+
+let cardInterval = setInterval(autoSlideCards, 6000);
+
+function resetSetInterval() {
+  clearInterval(cardInterval);
+  cardInterval = setInterval(autoSlideCards, 6000);
+}
